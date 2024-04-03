3c788def84fd9b3c8de9f17e3cdf79f37782451b
teestojko <158604040+teestojko@users.noreply.github.com>
2024-04-03 14:17:09 +0900

    入力ページレイアウト変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -148,7 +148,7 @@
           </div>
         </div>
         <div class="form__button">
-          <button class="form__button-submit" type="submit">送信</button>
+          <button class="form__button-submit" type="submit">確認画面</button>
         </div>
       </form>
     </div>

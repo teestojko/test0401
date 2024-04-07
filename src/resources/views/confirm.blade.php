911a9960efa1e3bf6071866fdadb83ce1a69d702
teestojko <158604040+teestojko@users.noreply.github.com>
2024-04-07 15:09:52 +0900

    “リモートリポジトリの変更”

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -32,10 +32,10 @@
               <th class="confirm-table__header">お名前</th>
               <td class="confirm-table__text">
                 <div style="display: inline-block;">
-                <input type="text" name="name" value=" {{ $contact['last_name'] }} " readonly/>
+                <input type="text" name="last_name" value=" {{ $contact['last_name'] }} " readonly/>
                 </div>
                 <div style="display: inline-block;">
-                <input type="text" name="name" value=" {{ $contact['first_name'] }} " readonly/>
+                <input type="text" name="first_name" value=" {{ $contact['first_name'] }} " readonly/>
                 </div>
               </td>
             </tr>
